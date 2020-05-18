26a5c093dbff1a9042a32a28c9771232c5603ddc
José Francisco Riffo <jfriffoa@gmail.com>
2020-05-18 09:31:58 -0400

    Initial Commit

---
+++ Assets/Scripts/FollowObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowObject : MonoBehaviour
+{
+    public Transform toFollow;
+
+    Vector3 _lastKnownPosition;
+    void Start() {
+        _lastKnownPosition = toFollow.position;
+    }
+
+    void LateUpdate()
+    {
+        var deltaPos = toFollow.position - _lastKnownPosition;
+        transform.position += deltaPos;
+        _lastKnownPosition = toFollow.position;
+    }
+}
