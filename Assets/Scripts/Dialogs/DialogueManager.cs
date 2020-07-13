601c06c1fdb19250320952b390e1b7bb9bde9077
José Francisco Riffo <jfriffoa@gmail.com>
2020-07-13 17:33:17 -0400

    Basic Dialog System

---
+++ Assets/Scripts/Dialogs/DialogueManager.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+namespace TFG.Dialog {
+    public class DialogueManager : MonoBehaviour {
+        public TextAsset rawFile;
+        public Text text;
+
+        public GameObject P1;
+        public GameObject P2;
+
+        List<Dialog> _dialogs;
+        int _dialogIndex = 0;
+
+        [Header("Callbacks")]
+        public UnityEvent onStart;
+        public UnityEvent onNextDialog;
+        public UnityEvent onFinish;
+        
+        // Start is called before the first frame update
+        IEnumerator Start() {
+            P1.SetActive(false);
+            P2.SetActive(false);
+
+            _dialogs = Parser.LoadFileFromString(rawFile.text);
+            yield return null;
+            OnLoad();
+        }
+
+        void OnLoad() {
+            text.text = _dialogs[_dialogIndex].Text;
+            onStart.Invoke();
+            UpdateAttributes();
+        }
+
+        public void Next() {
+            _dialogIndex++;
+            if (_dialogIndex >= _dialogs.Count) {
+                Debug.Log("Dialog reached to an end.");
+                onFinish.Invoke();
+            } else {
+                text.text = _dialogs[_dialogIndex].Text;
+                UpdateAttributes();
+                onNextDialog.Invoke();
+            }
+        }
+
+        void UpdateAttributes() {
+            switch (_dialogs[_dialogIndex].Attribute) {
+                case Dialog.DialogAttribute.P1:
+                    P1.SetActive(true);
+                    P2.SetActive(false);
+                    break;
+                case Dialog.DialogAttribute.P2:
+                    P1.SetActive(false);
+                    P2.SetActive(true);
+                    break;
+                case Dialog.DialogAttribute.None:
+                    P1.SetActive(false);
+                    P2.SetActive(false);
+                    break;
+                case Dialog.DialogAttribute.Both:
+                    P1.SetActive(true);
+                    P2.SetActive(true);
+                    break;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Dialogs/Parser.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.IO;
+
+namespace TFG.Dialog {
+    public class Dialog {
+        public enum DialogAttribute {
+            None, P1, P2, Both
+        }
+
+        DialogAttribute _attribute;
+        string _text;
+
+        public Dialog(DialogAttribute attribute, string text) {
+            _attribute = attribute;
+            _text = text;
+        }
+
+        public DialogAttribute Attribute { get => _attribute; }
+        public string          Text      { get => _text; }
+    }
+
+    public class Parser {
+        public static List<Dialog> LoadFileFromPath(string path) {
+            List<Dialog> dialogs = new List<Dialog>();
+
+            if (!File.Exists(path)) {
+                Debug.LogWarning(path + " File doesn't exists. Aborting...");
+                return dialogs;
+            }
+
+            using (var reader = new StreamReader(path)) {
+                while (!reader.EndOfStream) {
+                    var line = reader.ReadLine().Trim();
+                    dialogs.Add(ParseLine(line));
+                }
+            }
+
+            return dialogs;
+        }
+
+        public static List<Dialog> LoadFileFromString(string txt) {
+            List<Dialog> dialogs = new List<Dialog>();
+
+            if (string.IsNullOrEmpty(txt)) {
+                Debug.LogWarning("The string doesn't have data. Aborting...");
+                return dialogs;
+            }
+
+            using (var reader = new StringReader(txt)) {
+                string line;
+                while ((line = reader.ReadLine()) != null) {
+                    dialogs.Add(ParseLine(line));
+                }
+            }
+
+            return dialogs;
+        }
+
+        static Dialog ParseLine(string line) {
+            int idx = line.IndexOf(":");
+
+            var attr = ParseAttributes(line.Substring(0, idx));
+            var txt = line.Substring(idx + 1);
+
+            return new Dialog(attr, txt);
+        }
+
+        static Dialog.DialogAttribute ParseAttributes(string attributes) {
+            char[] trimChars = { '[', ']' };
+            string key = attributes.Trim(trimChars);
+
+            var attr = Dialog.DialogAttribute.None;
+
+            switch (key) {
+                case "P1": attr = Dialog.DialogAttribute.P1; break;
+                case "P2": attr = Dialog.DialogAttribute.P2; break;
+                case "P3": attr = Dialog.DialogAttribute.Both; break;
+            }
+
+            return attr;
+        }
+    }
+}
