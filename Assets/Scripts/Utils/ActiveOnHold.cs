67358c58c02612b6b654f626333d34776dd735d9
José Francisco Riffo <jfriffoa@gmail.com>
2020-07-27 19:38:51 -0400

    Memory 1 & 2 + Instructions

---
+++ Assets/Scripts/Utils/ActiveOnHold.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace TFG {
+    public class ActiveOnHold : MonoBehaviour, IPointerDownHandler, IPointerUpHandler {
+        public GameObject[] toActive;
+
+        void IPointerDownHandler.OnPointerDown(PointerEventData eventData) {
+            foreach (var go in toActive) {
+                go.SetActive(true);
+            }
+        }
+
+        void IPointerUpHandler.OnPointerUp(PointerEventData eventData) {
+            foreach (var go in toActive) {
+                go.SetActive(false);
+            }
+        }
+    }
+}
