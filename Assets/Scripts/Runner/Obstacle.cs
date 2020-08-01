10c2f0f8c4887f5f6fc9923f10a4edde49391e10
José Francisco Riffo <jfriffoa@gmail.com>
2020-07-31 20:02:35 -0400

    Runner Ground Tiles + Obstacles

--- Assets/Scripts/Runner/Obstacle.cs
+++ Assets/Scripts/Runner/Obstacle.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 namespace TFG.Runner {
     public class Obstacle : MonoBehaviour {
         public int score = 1;
+        public Color inactiveColor = Color.white;
         bool _dead;
 
         void LateUpdate() {
@@ -14,14 +15,21 @@ namespace TFG.Runner {
             if (transform.position.x < RunnerManager.Instance.player.transform.position.x) {
                 RunnerManager.Instance.AddScore(score);
                 _dead = true;
-                Destroy(gameObject, 1f);
+                Destroy(gameObject, 2f);
             }
         }
 
         void OnCollisionEnter2D(Collision2D collision) {
             if (collision.gameObject.CompareTag("Player")) {
                 RunnerManager.Instance.HitPlayer();
-                Destroy(gameObject);
+                //Destroy(gameObject);
+
+                // Deactive the object
+                GetComponent<SpriteRenderer>().color = inactiveColor;
+                var colliders = GetComponents<Collider2D>();
+                foreach (var collider in colliders) {
+                    collider.enabled = false;
+                }
             }
         }
     }
