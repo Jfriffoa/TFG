15c53c75ba77e2b9d27512927dedbe8e30b5cea3
José Francisco Riffo <jfriffoa@gmail.com>
2020-08-04 22:36:03 -0400

    Firebase + CTexture resizing

---
+++ Assets/Scripts/CheckGoogle.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckGoogle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task => {
+            var dependencyStatus = task.Result;
+            if (dependencyStatus == Firebase.DependencyStatus.Available) {
+                //Tamos de pana
+                Debug.Log(Firebase.FirebaseApp.DefaultInstance);
+            } else {
+                Debug.LogError("Could not resolve all firebase dependencies: " + dependencyStatus);
+            }
+        });
+    }
+}
